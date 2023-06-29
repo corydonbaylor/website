1a89213f28ec4304ddcad7bec26aebad4e863b47
Corydon Baylor <cwbaylor1@gmail.com>
2023-06-29 17:29:43 -0400

    welp

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello World2!"
+    return render_template('index.html', title='Home', color='blues', pad='p-0')
