40facac12456f14c2822255d9ca44d54b327ce7d
Corydon Baylor <cwbaylor1@gmail.com>
2023-10-07 15:23:58 -0400

    adding pokemon csv

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,13 +1,29 @@
 from flask import render_template
 from app import app
+from flask import Response
+import os
 
 # home route
+
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home', color='blues', pad='p-0')
 
 # javascript
+
+
 @app.route("/toggle_js")
 def toggle_js():
     return render_template("/javascript/toggles.js")
+
+# csv
+
+
+@app.route("/csv")
+def get_csv():
+    path = os.path.abspath(os.path.dirname(__file__)) + '/pokemon.csv'
+    with open(path, "r") as f:
+        content = f.read()
+    return Response(content, mimetype="text/plain")
