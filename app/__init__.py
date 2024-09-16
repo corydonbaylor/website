f9a63a9e5e35bf08f5522f1b7c94f9007e469a09
Corydon Baylor <cwbaylor1@gmail.com>
2024-09-16 02:15:15 -0400

    no content

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 from app.routes import routes
 from app.routes import routes_coding
-from app.routes import routes_tutorials
-from app.routes import routes_viz
-from app.routes import routes_travel
-from app.routes import routes_statistics
+# from app.routes import routes_tutorials
+# from app.routes import routes_viz
+# from app.routes import routes_travel
+# from app.routes import routes_statistics
